a5868d5e090cfddfe7aeb557dee5d1b0e7362287
Faststylemale <00>
2019-08-29 10:28:07 +0800

    提交

---
+++ src/main/java/com/itheima/User.java
@@ -0,0 +1,29 @@
+package com.itheima;
+
+/**
+ * 工程: userGit
+ * 包名: com.itheima
+ * 描述: TODO
+ * 编程: admin
+ * 日期 2019/8/29 10:26
+ */
+public class User {
+    private String name;
+    private  String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
